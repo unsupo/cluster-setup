9adb64b85c39b00c3976675ca5bd3045e1d80260
Jonathan Arndt <jarndt@demandware.com>
2018-11-29 17:19:05 -0700

    fixed, added relative path

--- master_scripts/vagrant_master_init.sh
+++ master_scripts/vagrant_master_init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git clone --single-branch -b master-setup https://github.com/unsupo/cluster-setup.git \
     && cd cluster-setup \
-    && sh /master_scripts/master_script.sh
+    && sh ./master_scripts/master_script.sh
