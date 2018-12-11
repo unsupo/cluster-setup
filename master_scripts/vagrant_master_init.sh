c04e67d18a59a4d1b57e338205ed0f2829c8c111
Jonathan Arndt <jarndt@demandware.com>
2018-12-11 12:15:41 -0700

    local network on 10.0.0.* adding sudo to running command

--- master_scripts/vagrant_master_init.sh
+++ master_scripts/vagrant_master_init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git clone --single-branch -b master-setup https://github.com/unsupo/cluster-setup.git \
     && cd cluster-setup \
-    && sh ./master_scripts/master_script.sh 192.168.50.0
+    && sudo sh ./master_scripts/master_script.sh 192.168.50.0
