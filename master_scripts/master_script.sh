3ca997a65846a7a8d24e42c3a94cf2eda1d523ae
Jonathan <unsupo@gmail.com>
2018-12-10 22:52:13 -0700

    Update master_script.sh

--- master_scripts/master_script.sh
+++ master_scripts/master_script.sh
@@ -87,7 +87,7 @@ mv $f /etc/salt/roster
 # all hosts exist in salt-ssh, so install the salt-minion on them and set up there minion id
 # salt-ssh -i
 # can't ssh-salt '*' state.apply salt.minion because https://github.com/saltstack/salt/issues/21370
-salt-ssh '*' cmd.run "echo `hostname -I` > /root/master_ip"
+salt-ssh '*' cmd.run "echo `hostname -I | awk '{print $1}'` > /root/master_ip"
 salt-ssh '*' state.apply salt-bootstraper.minion
 
 # salt-key -A -y
